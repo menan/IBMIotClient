a65e354b4467d250e1fdbdbce82dfb32f66c1d3c
Menan Vadivel <menan_vadivelpillai@intuit.com>
2019-04-02 18:26:58 -0400

    Initial Commit

---
+++ IBMIoTClient/IBMIoTClient.h
@@ -0,0 +1,19 @@
+//
+//  IBMIoTClient.h
+//  IBMIoTClient
+//
+//  Created by Vadivelpillai, menan on 4/2/19.
+//  Copyright © 2019 Tinrit Labs Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IBMIoTClient.
+FOUNDATION_EXPORT double IBMIoTClientVersionNumber;
+
+//! Project version string for IBMIoTClient.
+FOUNDATION_EXPORT const unsigned char IBMIoTClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IBMIoTClient/PublicHeader.h>
+
+
